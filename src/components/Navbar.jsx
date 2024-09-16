37e88dc740c5c1ea495761d0d4ee616242764fdf
Muhammed Shibil M <95218558+muhammedshibilm@users.noreply.github.com>
2024-09-16 13:21:49 +0530

    changed body colour

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { FiMenu, FiX } from "react-icons/fi";
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
     const [toggle, setToggle] = useState(false);
@@ -19,16 +20,26 @@ const Navbar = () => {
         setToggle(false); // Close the menu after clicking
     };
 
+    const varients = {
+        hidden: { opacity: 0},
+        visible: (i)=>({
+            opacity: 1,
+            transition: {
+                delay: i * 0.3
+            }
+        })
+    }
+
     return (
         <nav className="fixed top-0 left-0 w-full flex justify-between items-center p-6 z-50">
-            <h1 className="md:text-3xl text-xl">Muhammed Shibil</h1>
-            <ul className="hidden md:flex gap-4">
-                {links.map((item) => (
-                    <li key={item.id} className="cursor-pointer list-none hover:bg-gray-200 py-2 px-4 rounded-md">
+            <motion.h1 initial={{opacity: 0,scale: 0.5}} animate={{opacity: 1, scale: 1}} transition={{duration: 0.5}} className="md:text-3xl text-xl">Muhammed Shibil</motion.h1>
+            <motion.ul className="hidden md:flex gap-4" variants={varients} initial={"hidden"} animate={"visible"}>
+                {links.map((item,index) => (
+                    <motion.li key={item.id} className="cursor-pointer list-none hover:bg-gray-200 py-2 px-4 rounded-md" variants={varients} custom={index} >
                         <a href={item.href} onClick={(e) => handleClick(e, item.href)}>{item.name}</a>
-                    </li>
+                    </motion.li>
                 ))}
-            </ul>
+            </motion.ul>
             <div className="md:hidden">
                 {toggle ? (
                     <FiX
@@ -49,13 +60,13 @@ const Navbar = () => {
                 )}
             </div>
             {toggle && (
-                <ul className="flex flex-col justify-center items-center fixed top-0 left-0 h-screen w-full bg-white text-black">
-                    {links.map((item) => (
-                        <li key={item.id} className="cursor-pointer py-4 capitalize text-2xl hover:text-gray-500 ">
+                <motion.ul className="flex flex-col justify-center items-center fixed top-0 left-0 h-screen w-full bg-white text-black" variants={varients} initial={"hidden"} whileInView={"visible"}>
+                    {links.map((item,index) => (
+                        <motion.li key={item.id} className="cursor-pointer py-4 capitalize text-2xl hover:text-gray-500 " variants={varients} custom={index}>
                             <a href={item.href} onClick={(e) => handleClick(e, item.href)}>{item.name}</a>
-                        </li>
+                        </motion.li>
                     ))}
-                </ul>
+                </motion.ul>
             )}
         </nav>
     );
